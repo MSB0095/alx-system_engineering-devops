c1e75089a12a38dde3403cf70ab8561b1a2ebdbf
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-08-15 16:39:53 +0100

    refactor: Update number_of_subscribers function to return an integer value

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -27,10 +27,10 @@ def number_of_subscribers(subreddit: str) -> int:
         if 'error' in data.keys():
             return 0
         else:
-            return data['data']['subscribers']
+            return int(data['data']['subscribers'])
     except Exception as e:
         return 0
 
 
 if __name__ == "__main__":
-    print(number_of_subscribers(argv[1]))
+    print(int(number_of_subscribers(argv[1])))
