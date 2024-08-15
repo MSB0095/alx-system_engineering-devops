85ac9b3b1a9cdc6cc4b978a8c44ce7fa2920df26
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-08-15 15:59:41 +0100

    feat: Add Python script to get number of subscribers on a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Query Reddit API to get the number
+of subscribers on a subreddit"""
+from requests import get
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """return the number of subscribers"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'MSB0095'}
+
+    response = get(url, headers=headers)
+    data = response.json()
+
+    if response.status_code == 200:
+        return data['data']['subscribers']
+    else:
+        return 0
