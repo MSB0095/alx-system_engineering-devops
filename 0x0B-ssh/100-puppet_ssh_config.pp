1e9c7627ecb53567ab61fe944752ac96290f370b
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-08-17 16:26:45 +0100

    feat: Configure SSH client to use private key and disable password authentication

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# This Puppet manifest configures the SSH client to use the private key ~/.ssh/school
+# and to refuse password authentication.
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^    IdentityFile',
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^    PasswordAuthentication',
+}
